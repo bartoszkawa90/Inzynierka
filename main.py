098c674f70b0e405328ade9356b6cbaddfc3751a
Bartosz Kawa <bartekkawa2@gmail.com>
2023-04-14 14:15:17 +0200

    14.04

--- main.py
+++ main.py
@@ -19,54 +19,62 @@ from matplotlib import pyplot as plt
 
 # JESLI JUZ MACHINE LEARNING TO JEST COS W OPENCV  +  TensorFlow
 
-def plot_photo(title, image):
+def plot_photo(title, image,height, widht):
+    ''' Plots photo is given  resolution
+        title - title of ploted photo
+        image - image to plot
+        height - height of ploted photo
+        width - width of ploted photo
+    '''
     while True:
+        cv.namedWindow(title, cv.WINDOW_NORMAL)
+        cv.resizeWindow(title, height, widht)
         cv.imshow(title, image)
         cv.waitKey(0)
         sys.exit()
-    cv.destroyAllWindows()
+        cv.destroyAllWindows()
 
+
+
+if __name__ == '__main__':
+    print("Start")
+
+    # Reading an image in default mode
+    image = cv.imread('Zdjecia/Ki-67 60%.jpg')
+    a = np.zeros(30)
+    print(a)
+    # Display photo
+    # plot_photo("Photo",image,1000,1000)
+
+
+# # Load two images
+# gekon = cv.imread('photo.jpg')
+# logo = cv.imread('pobrane.png')
+# assert gekon is not None, "file could not be read, check with os.path.exists()"
+# assert logo is not None, "file could not be read, check with os.path.exists()"
 #
-# if __name__ == '__main__':
-#     print("Start")
+# # I want to put logo on top-left corner, So I create a ROI
+# rows,cols,channels = logo.shape
+# roi = gekon[0:rows, 0:cols]
 #
-#     image1 = cv.imread('photo.jpg')
-#     image2 = cv.imread('pobrane.png')
+# # Now create a mask of logo and create its inverse mask also
+# img2gray = cv.cvtColor(logo, cv.COLOR_BGR2GRAY)
+# ret, mask = cv.threshold(img2gray, 175, 255, cv.THRESH_BINARY_INV)  # daje białe logo
+# mask_inv = cv.bitwise_not(mask)   # daje bieałe tło loga
 #
-#     image2 = cv.resize(image2, (800,600))
+# # # Wycina ANDem obramówke do logo z wycinka całego zdjecia gekona
+# img1_bg = cv.bitwise_and(roi,roi,mask = mask_inv)
 #
-#     image = cv.addWeighted(image1,0.7,image2,0.3,0.0)
+# # # wicina ze zdjecia loga samo logo bez tła
+# img2_fg = cv.bitwise_and(logo, logo, mask = mask)
 #
-#     plot_photo("Photo", image)
-
-# Load two images
-gekon = cv.imread('photo.jpg')
-logo = cv.imread('pobrane.png')
-assert gekon is not None, "file could not be read, check with os.path.exists()"
-assert logo is not None, "file could not be read, check with os.path.exists()"
-
-# I want to put logo on top-left corner, So I create a ROI
-rows,cols,channels = logo.shape
-roi = gekon[0:rows, 0:cols]
-
-# Now create a mask of logo and create its inverse mask also
-img2gray = cv.cvtColor(logo, cv.COLOR_BGR2GRAY)
-ret, mask = cv.threshold(img2gray, 175, 255, cv.THRESH_BINARY_INV)  # daje białe logo
-mask_inv = cv.bitwise_not(mask)   # daje bieałe tło loga
-
-# # Wycina ANDem obramówke do logo z wycinka całego zdjecia gekona
-img1_bg = cv.bitwise_and(roi,roi,mask = mask_inv)
-
-# # wicina ze zdjecia loga samo logo bez tła
-img2_fg = cv.bitwise_and(logo, logo, mask = mask)
-
-# # skleja tył ze fragmentu zdjecia gekona i logo ze zdjecia z logiem
-dst = cv.add(img1_bg,img2_fg)
-gekon[0:rows, 0:cols] = dst
-plot_photo('photo',gekon)
-
-
-# cv.imshow('res',img1)
-# cv.waitKey(0)
-# cv.destroyAllWindows()
+# # # skleja tył ze fragmentu zdjecia gekona i logo ze zdjecia z logiem
+# dst = cv.add(img1_bg,img2_fg)
+# gekon[0:rows, 0:cols] = dst
+# plot_photo('photo',gekon)
+#
+#
+# # cv.imshow('res',img1)
+# # cv.waitKey(0)
+# # cv.destroyAllWindows()
 
