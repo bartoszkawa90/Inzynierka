5d5ca057d6e7e20a86c508e5c3a8d103b42104f6
Bartosz Kawa <bartekkawa2@gmail.com>
2023-05-07 02:40:01 +0200

    7.05

--- Notes.py
+++ Notes.py
@@ -1,26 +1,21 @@
 # Notes
 
-# from resources import *
-import sys
+from resources import *
 
-# cell = cv2.imread('example_cell.jpg')
-# gray = cv2.cvtColor(cell, cv2.COLOR_BGRA2GRAY)
-#
-# lista = [1,2,3,4]
-# print(lista)
-# lista.clear()
-# print(lista)
+
+
+
 
 
 # check time
-import time
-start_time = time.time()
-print(sys.version)
-a = 0
-for i in range(10000000):
-    a = i +3 *2 - 1.5*i
-print(a)
-print("--- %s seconds ---" % (time.time() - start_time))
+# import time
+# start_time = time.time()
+# print(sys.version)
+# a = 0
+# for i in range(10000000):
+#     a = i +3 *2 - 1.5*i
+# print(a)
+# print("--- %s seconds ---" % (time.time() - start_time))
 
 
 # DISPLAY
