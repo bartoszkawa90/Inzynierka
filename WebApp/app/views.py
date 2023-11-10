c6a3bacec3c87bf19700ceac5630443839adfbfb
Bartosz_Kawa <bartekkawa2@gmail.com>
2023-11-10 10:16:23 +0100

    Da sie wysyłać zdjecie ale trzeba dorobic zeby sie wyswietlalo

--- WebApp/app/views.py
+++ WebApp/app/views.py
@@ -3,28 +3,24 @@ from django.conf import settings
 import os
 
 acceptable_extensions = ['jpg', 'jpeg', 'pdf', 'png']
+context = {
+    'image_name': ''
+}
 
 # Create your views here.
 def home(request):
     if request.method == 'POST':
-
-        # print(f'Received files \n {request.FILES}')
-
-        # if request.FILES["file_upload"].name.split('.')[1] not in acceptable_extensions:
-        #     print('[ERROR] Wrong type of file were selected')
-        # else:
-        ## SAVE IMAGE WITH ITS NAME
-        # save_path = os.path.join(settings.MEDIA_ROOT,
-        #                          request.FILES["file_upload"].name)
-
         ## ZAPISUJEMY Z TA SAMĄ NAZWĄ ZEBY NADPISAĆ STARE ZDJECIE
         save_path = 'image.jpg'
+        image_name = request.FILES["file_upload"].name
 
         with open(save_path, "wb") as output_file:
             for chunk in request.FILES["file_upload"].chunks():
                 output_file.write(chunk)
+        ## CREATE CONTEXT
+        context['image_name'] = image_name
 
-    return render(request, 'homepage.html')
+    return render(request, 'homepage.html', {'context': context})# {'image_name': image_name})
 
 def doc(request):
     return render(request, 'documentation.html')
