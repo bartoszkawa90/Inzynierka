0b284ab578bceb6018fd60ac64a172798cae42c4
Bartosz_Kawa <bartekkawa2@gmail.com>
2023-11-23 21:37:17 +0100

    aaa

--- WebApp/WebApp/settings.py
+++ WebApp/WebApp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4*56k@u$a(!0w9rpgovv%^=4fw3h$khw6#y@td@y=wtbv_lnci
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,8 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'app/static'), ]
-# STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_URL = '/media/'
