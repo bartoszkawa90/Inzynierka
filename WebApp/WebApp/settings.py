69bb99f5ec727dd22932362363509538507ee65c
Bartosz_Kawa <bartekkawa2@gmail.com>
2023-11-23 22:07:37 +0100

    a

--- WebApp/WebApp/settings.py
+++ WebApp/WebApp/settings.py
@@ -14,7 +14,9 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -124,6 +126,7 @@ STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_URL = '/media/'
+MEDIA_ROOT = '/home/your_username/your_project_name/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
